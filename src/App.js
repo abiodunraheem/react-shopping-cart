9803f4bc87145c865002228a30fda0ed93cd8b4b
abiodunraheem <raheemabiodun23@gmail.com>
2022-08-13 12:07:05 +0100

    Create feature-1 branch

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          Products List
+        </main>
+        <footer>
+          All rigth is reserved &copy; 2022
+        </footer>
+    </div>
+  );
+}
+
+export default App;
